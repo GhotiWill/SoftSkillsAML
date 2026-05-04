47c2650103f801ef54aed0e9356361a0f13db4f4
GhotiWill <ghoti.will@gmail.com>
2026-05-04 19:01:10 +0300

    Refine button alignment and move navigation into sidebars

--- SoftSkillsAML/Converters/BoolToBorderBrushConverter.cs
+++ SoftSkillsAML/Converters/BoolToBorderBrushConverter.cs
@@ -7,9 +7,12 @@ namespace SoftSkillsAML.Converters
 {
     internal class BoolToBorderBrushConverter : IValueConverter
     {
+        private static readonly SolidColorBrush CompletedBrush = new(Color.Parse("#44a08d"));
+        private static readonly SolidColorBrush PendingBrush = new(Color.Parse("#764ba2"));
+
         public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
         {
-            return value is bool b && b ? Brushes.Green : Brushes.Red;
+            return value is bool b && b ? CompletedBrush : PendingBrush;
         }
 
         public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
